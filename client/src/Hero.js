8595c6f996fb61c5ca44b95075ab716cd93f3076
Emily Cook <ecook@launchcg.com>
2020-04-26 13:04:59 -0700

    feat: photo credit

--- client/src/Hero.js
+++ client/src/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
 import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
+import { Link, Typography } from '@material-ui/core'
 import clsx from 'clsx'
 
 import WidthContainer from './WidthContainer'
@@ -50,6 +50,15 @@ const useStyles = makeStyles(theme => ({
     justifyContent: 'center',
     alignItems: 'center',
     alignContent: 'center'
+  },
+  photoCredit: {
+    textAlign: 'right',
+    fontSize: 10,
+    marginRight: 10,
+    cursor: 'pointer'
+  },
+  link: {
+    color: '#afaeae'
   }
 }))
 
@@ -75,23 +84,35 @@ function Hero({ heroImg, grayscale = '34%', subText, title }) {
   const heroStyles = useHeroStyles()
 
   return (
-    <div className={clsx(classes.root, heroStyles.heroImageStyle)}>
-      {title && (
-        <WidthContainer fullWidth={true} className={classes.columnWrapper}>
-          <Grid container spacing={0} className={classes.container}>
-            <Grid item md={5} sm={6} xs={11} className={classes.heroOverlay}>
-              <Typography variant="h2" className={classes.heroTitle}>
-                {title}
-              </Typography>
-              {subText && (
-                <Typography variant="body2" className={classes.subTitle}>
-                  {subText}
+    <div>
+      <div className={clsx(classes.root, heroStyles.heroImageStyle)}>
+        {title && (
+          <WidthContainer fullWidth={true} className={classes.columnWrapper}>
+            <Grid container spacing={0} className={classes.container}>
+              <Grid item md={5} sm={6} xs={11} className={classes.heroOverlay}>
+                <Typography variant="h2" className={classes.heroTitle}>
+                  {title}
                 </Typography>
-              )}
+                {subText && (
+                  <Typography variant="body2" className={classes.subTitle}>
+                    {subText}
+                  </Typography>
+                )}
+              </Grid>
             </Grid>
-          </Grid>
-        </WidthContainer>
-      )}
+          </WidthContainer>
+        )}
+      </div>
+      <Typography className={classes.photoCredit}>
+        <Link
+          className={classes.link}
+          href="https://www.instagram.com/erik.aleksandrovich/?hl=en"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          *Photo by Erik Aleksandrovich
+        </Link>
+      </Typography>
     </div>
   )
 }
