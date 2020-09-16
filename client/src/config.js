33c4d2d286ff6a5130cdf908eef168322b8d2e49
Emily Cook <emilycook@embook.local>
2020-09-16 08:47:47 -0700

    fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 export default {
   VERSION: process.env.REACT_APP_VERSION,
+  ENV: process.env.REACT_APP_ENV,
   USPS_USER_ID: process.env.REACT_APP_USPS_USER_ID || '675SHELB3149',
   API: process.env.REACT_APP_API || 'https://us-central1-shelbyart-8c287.cloudfunctions.net/app'
 };
